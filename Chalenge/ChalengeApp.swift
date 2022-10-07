8e9b36bd3aab6ed6292f8d94e5c99e68fae05511
Fhikar Zul <Fhikarzul18@gmail.com>
2022-10-07 10:10:11 +0800

    Initial Commit

---
+++ Chalenge/ChalengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChalengeApp.swift
+//  Chalenge
+//
+//  Created by Lucy on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ChalengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
